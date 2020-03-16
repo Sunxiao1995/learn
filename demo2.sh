d88d120ca74c5769894a86a288aebe04f8e2749d
Sunxiao1995 <1406884273@qq.com>
2020-03-16 23:44:42 +0800

    git练习2

--- demo2.sh
+++ demo2.sh
@@ -5,7 +5,7 @@ mv ex01.c example
 mv example/ex01.c example/example.c
 mkdir practice
 mv ex02.c practice
-mv practice/ex02.c practice/ex03.c
+mv practice/ex02.c practice/practice.c
 rm ex03.c
 echo '## example/example.c  
 判断是奇数还是偶  
