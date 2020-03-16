6c1e13c666003555ea98f78b9b29e2c4bc16f0ce
Sunxiao1995 <1406884273@qq.com>
2020-03-16 16:58:15 +0800

    git练习二

---
+++ demo2.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+touch ex01.c ex02.c ex03.c
+mkdir example
+mv ex01.c example
+mv example/ex01.c example/example.c
+mkdir practice
+mv ex02.c practice
+mv practice/ex02.c practice/ex03.c
+rm ex03.c
+echo '## example/example.c  
+判断是奇数还是偶  
+## practice/practice.c  
+比较两个数大小' > README.md
+git add .
+git commit -m"提交所有改动"
+git push 
+
+
