86a23e53f8fac7fdf421930407a2b6b689eaf3b9
Sunxiao1995 <1406884273@qq.com>
2020-03-16 16:55:38 +0800

    修改“

--- demo.sh
+++ demo.sh
@@ -1,9 +1,10 @@
-mkdir ~/prepare/practice/new
-touch ~/prepare/practice/new/new.txt
-echo "welcome to banyuan">~/prepare/practice/new/new.txt
-mkdir -p ~/prepare/practice/newother/sub
-cp ~/prepare/practice/new/new.txt ~/prepare/practice/newother/sub
-mv ~/prepare/practice/new/new.txt ~/prepare/practice/new/new1.txt
-mv ~/prepare/practice/newother/sub ~/prepare/practice
-mv ~/prepare/practice/sub ~/prepare/practice/new2
-rm -rf ~/prepare/practice/new*
+#! /bin/zsh
+mkdir new
+touch new/new.txt
+echo "welcome to banyuan" > new/new.txt
+mkdir -p newother/sub
+cp new/new.txt newother/sub
+mv new/new.txt new/new1.txt
+mv newother/sub .
+mv sub new2
+find ./ -type d -name "new*" -print0 |xargs -0 rm -r
