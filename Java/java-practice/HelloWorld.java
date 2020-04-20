d72315ef0ed0ef0647b8f5a89d7b99988c113871
Sunxiao1995 <1406884273@qq.com>
2020-04-20 12:17:56 +0800

    java

---
+++ Java/java-practice/HelloWorld.java
@@ -0,0 +1,45 @@
+/*
+1、cd learn/java-practice
+
+Javac Hello.java
+
+ls
+
+Java Hello
+
+rm Hello.class
+
+open Hello.java
+
+java test
+
+java Test
+
+mv test.class test1.class
+
+mv test1.class TEST.class
+
+java Test
+
+2、生成的文件名是test.class
+
+java test
+
+3、未生成新的Class 文件
+
+​     Java Test
+
+​    无法运行
+
+​    可以运行
+*/
+
+
+
+
+class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
---
+++ c-node/Untitled.md
@@ -0,0 +1,8 @@
+Source -./bash_profile
+
+环境变量改完以后生效
+
+
+
+export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+
