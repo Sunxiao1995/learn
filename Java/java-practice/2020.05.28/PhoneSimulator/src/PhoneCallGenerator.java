629127afbaf9d830cab7d36dfaaaf4bc333515a0
Sunxiao1995 <1406884273@qq.com>
2020-05-28 23:38:50 +0800

    2020.05.28

--- Java/java-practice/2020.05.28/PhoneSimulator/src/PhoneCallGenerator.java
+++ Java/java-practice/2020.05.28/PhoneSimulator/src/PhoneCallGenerator.java
@@ -6,7 +6,7 @@ import java.util.Random;
  */
 public class PhoneCallGenerator extends Thread {
 
-  private CellularPhone phone;
+  private final CellularPhone phone;
 
   public PhoneCallGenerator(String name, CellularPhone thePhone) {
     super(name);
@@ -22,9 +22,9 @@ public class PhoneCallGenerator extends Thread {
     try {
       while (counter++ < 20) {
         // 随机生成0到3秒之间的呼叫长度
-        synchronized (phone) {
           int length = randomGenerator.nextInt(maxCallLength);
 
+          synchronized (phone) {
           // 试着发起一个呼叫
           phone.startCall(getName(), Integer.toString(counter));
           // 呼叫开始，休眠随机生成的时间，然后结束通话
